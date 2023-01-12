3d4a7851ea7ca77dd3c4b65633c165b1749d0e10
Sean <seanboateng2@gmail.com>
2023-01-12 12:03:01 -0500

    Data from video ganmes sales since 2013 is now displayed on chart

--- src/Component/GlobalSalesChart/GlobalSalesChart.jsx
+++ src/Component/GlobalSalesChart/GlobalSalesChart.jsx
@@ -3,21 +3,30 @@ import { Chart } from "react-google-charts";
 import axios from "axios";
 
 const GlobalSalesChart = (props) => {
-
-    const [platformSales, setPlatformSales] = useState([])
+  const[chartData,setChartData] = useState([])
+  const [platformSales, setPlatformSales] = useState([])
 
     useEffect(() => {
-        GetAllGames();
-      });
+        getPlatformSales();
+      },[]);
 
 
-    async function GetAllGames(){
+    async function getPlatformSales(){
         let response = await axios.get('http://localhost:8080/platformsales')
-        debugger
-        setPlatformSales(response.data)
-        console.log(platformSales)
         
+        formatData(response.data)
+    }
 
+    const formatData = (data)=>{
+        let formattedData = Object.entries(data).map(([key,value])=>{
+          console.log("Key: ", key, " Value: ", value)
+          // setChartData(...[key,value,"#b87333"])
+          
+          return([key, value, "#b87333"])
+          
+        })
+        console.log(formattedData)
+        setChartData([["Platform", "Sales", { role: "style" }], ...formattedData])
     }
 
     const data = [
@@ -31,7 +40,10 @@ const GlobalSalesChart = (props) => {
 
 
     return ( 
-        <Chart chartType="ColumnChart" width="100%" height="400px" data={data} />
+      <div>
+        {console.log("Chart Data", chartData)}
+        <Chart chartType="ColumnChart" width="100%" height="400px" data={chartData} />
+      </div>
      );
 }
  
